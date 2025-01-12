309252295f6c440f3a1ad32b0f57a063ffebe284
Kirill Barkov <freedomdragon777@gmail.com>
2025-01-13 01:08:47 +0400

    Спойлер готов.

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,20 @@
-var acc = document.getElementsByClassName("fomod__toggle");
+var fomodToggle = document.getElementsByClassName("fomod__toggle");
 var i;
 
-for (i = 0; i < acc.length; i++) {
-	acc[i].addEventListener("click", function() {
+for (i = 0; i < fomodToggle.length; i++) {
+	fomodToggle[i].addEventListener("click", function() {
 		this.classList.toggle("active");
 
 		var panel = this.nextElementSibling;
-		if (panel.style.display === "block") {
-			panel.style.display = "none";
-			panel.style.maxHeight = null;
-		} else {
+		if (panel.style.maxHeight === '' || panel.style.maxHeight === '0px') {
 			panel.style.display = "block";
 			panel.style.maxHeight = panel.scrollHeight + "px";
+		} else {
+			panel.style.maxHeight = '0px';
+			panel.addEventListener("transitionend", function handler() {
+				panel.style.display = "none";
+				panel.removeEventListener("transitionend", handler);
+			});
 		}
 	});
 }
